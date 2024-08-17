9208f1af1c2768cf9b26c692a1a277f50e835edb
Nekcilo <132229704+Nekcilo@users.noreply.github.com>
2024-08-17 18:59:54 +0100

    Print score in debug

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -8,6 +8,3 @@ func _input(event):
 	if event.is_action_pressed("Escape"):
 		get_tree().quit()
 		print("Quit Game")
-
-func _on_next_pressed():
-	get_tree().change_scene_to_file("res://dialogue.tscn")
--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -4,7 +4,7 @@ func _ready():
 	AudioPlayer.play_music_menu()
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://dialogue.tscn")
+	get_tree().change_scene_to_file("res://DiaGame.tscn")
 
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://OptionsMenu.tscn")
