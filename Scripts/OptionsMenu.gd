3dfaa8bda5e3636e96ce2e5277a2906de7faae59
Nekcilo <132229704+Nekcilo@users.noreply.github.com>
2024-08-18 08:20:06 +0100

    Res + Fullscreen

--- Scripts/OptionsMenu.gd
+++ Scripts/OptionsMenu.gd
@@ -1,7 +1,39 @@
 extends Control
 
+#var resolutions = {
+	#"2560 x 1440": Vector2i(2560, 1440),
+	#"1920 x 1080": Vector2i(1920, 1080),
+	#"1280 x 720": Vector2i(1280, 720),
+	#"1024 x 600": Vector2i(1024, 600),
+	#"800 x 600": Vector2i(800, 600)
+#}
+
 func _ready():
 	AudioPlayer.play_music_menu()
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://MainMenu.tscn")
+
+func _on_resolutions_item_selected(index):
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(2560, 1440))
+			print("2560 x 1440")
+		1:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+			print("1920 x 1080")
+		2:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+			print("1280 x 720")
+		3:
+			DisplayServer.window_set_size(Vector2i(1024, 600))
+			print("1024 x 600")
+		4:
+			DisplayServer.window_set_size(Vector2i(800, 600))
+			print("800 x 600")
+
+func _on_check_box_toggled(toggled_on):
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
--- Scripts/object.gd
+++ Scripts/object.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 var draggable = false
-#removed var is_inside_droppable = false
-#removed var body_ref
 var offset: Vector2
 var initialPos: Vector2
 var array_of_bodies = [] #added
@@ -15,18 +13,15 @@ func _process(delta):
 			offset = get_global_mouse_position() - global_position
 			Global.is_dragging = true
 			array_of_bodies.clear()
-			#ADDED to clear array if the original location was added to the array when using tween back to the initialPos
 			
 		if Input.is_action_pressed("Click"):
 			global_position = get_global_mouse_position()
 		elif Input.is_action_just_released("Click"):
 			Global.is_dragging = false
 			var tween = get_tree().create_tween()
-			#Removed if is_inside_droppable: tween.tween_property(self,"position", body_ref.position, 0.2).set_ease(Tween.EASE_OUT)
-			#removed else: tween.tween_property(self,"global_position", initialPos, 0.2).set_ease(Tween.EASE_OUT)
-			if not array_of_bodies.is_empty(): #ADDED
-				tween.tween_property(self,"position",array_of_bodies.back().position,0.2).set_ease(Tween.EASE_OUT) #ADDED
-				array_of_bodies.clear() #ADDED
+			if not array_of_bodies.is_empty():
+				tween.tween_property(self,"position",array_of_bodies.back().position,0.2).set_ease(Tween.EASE_OUT)
+				array_of_bodies.clear()
 			else:
 				tween.tween_property(self,"global_position",initialPos,0.2).set_ease(Tween.EASE_OUT) #NOTE this will add the original body to the array
 
@@ -43,14 +38,11 @@ func _on_area_2d_mouse_exited():
 func _on_area_2d_body_entered(body: StaticBody2D):
 	if body.is_in_group('droppable') && occupied == false:
 		occupied = true
-		#removed is_inside_droppable = true
 		array_of_bodies.append(body) #ADDED
 		body.modulate = Color(Color.html("000000"), 1)
-		#removed body_ref = body
 
 func _on_area_2d_body_exited(body):
 	if body.is_in_group('droppable'):
 		occupied = false
-		#REMOVED is_inside_dropable = false
 		array_of_bodies.erase(body) #ADDED
 		body.modulate = Color(Color.html("000000"), 0.7)
